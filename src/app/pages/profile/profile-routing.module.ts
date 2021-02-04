ff76880b5974eccf60c4eba366d0fed4e155b24e
Gabriel Mendes de Souza Santiago <gabriel.santiago@tqi.com.br>
2021-02-04 17:54:55 -0300

    profile page done

--- src/app/pages/profile/profile-routing.module.ts
+++ src/app/pages/profile/profile-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
   {
     path: '',
     children: [
-      { path: '', component: ProfileComponent },
+      { path: '', component: ProfileComponent, resolve: { profile: ProfileResolver } },
       { path: 'edit', component: EditProfileComponent, resolve: { profile: ProfileResolver } }
     ]
   }
--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Child, Sex } from './edit-profile/general-info-form/child-form/child.model';
+import { Profile } from './edit-profile/profile.model';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +10,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  profile!: Profile;
+
+  constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.profile = this.activatedRoute.snapshot.data.profile;
+    console.log(this.profile)
   }
 
 }
