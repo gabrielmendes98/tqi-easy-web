b6a743979ad2e158b7cb738ab11a53266d10b4ef
Gabriel Mendes de Souza Santiago <gabriel.santiago@tqi.com.br>
2021-02-04 16:19:30 -0300

    get router parameters in announcement

--- src/app/pages/announcements/announcement/announcement.component.ts
+++ src/app/pages/announcements/announcement/announcement.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-announcement',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./announcement.component.scss']
 })
 export class AnnouncementComponent implements OnInit {
+  id?: string;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.id = this.activatedRoute.snapshot.params.id;
   }
 
 }
