38115b3e4021dcdff791ab44af9bd1e809d16615
Gabriel Mendes de Souza Santiago <gabriel.santiago@tqi.com.br>
2021-03-05 09:45:37 -0300

    first color refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { OverlayContainer } from '@angular/cdk/overlay';
 
 import { UserService } from './core/user/user.service';
-import { Observable } from 'rxjs';
 import { User } from './core/user/user.model';
 import { LoadingService } from './core/loading/loading.service';
 import { SpinnerOverlayService } from './shared/components/spinner-overlay/spinner-overlay.service';
@@ -9,18 +10,25 @@ import { SpinnerOverlayService } from './shared/components/spinner-overlay/spinn
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   user$!: Observable<User | null>;
+  themeClass!: string;
 
-  constructor(private userService: UserService, private loadingService: LoadingService, private spinnerOverlayService: SpinnerOverlayService) { }
+  constructor(
+    private userService: UserService,
+    private loadingService: LoadingService,
+    private spinnerOverlayService: SpinnerOverlayService,
+    private overlayContainer: OverlayContainer
+  ) {}
 
   ngOnInit(): void {
+    this.overlayContainer.getContainerElement().classList.add('light-theme');
+
     this.user$ = this.userService.getUser();
-    this.loadingService.isNavigationPending$.subscribe(isLoading => {
+    this.loadingService.isNavigationPending$.subscribe((isLoading) => {
       isLoading ? this.spinnerOverlayService.showLoading() : this.spinnerOverlayService.hide();
     });
-
   }
 }
